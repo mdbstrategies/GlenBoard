2ffd6e07f349c2e8963a5061462d6186fef1640d
Webmaster <macshvot@gmail.com>
2020-12-22 13:03:19 -0300

    Hello World

---
+++ practice.js
@@ -0,0 +1,10 @@
+console.log ("Hello World");
+const Hello = {Sam: "Hello World"}
+console.log (Hello)
+let Something = Hello;
+Something.Sam = "Practicing"
+console.log (Hello)
+const List = [1,2,3,4,5,"Hello", {Joao: 'message'}]
+console.log (List)
+console.log (List [0])
+console.log (List [List.length -1])
